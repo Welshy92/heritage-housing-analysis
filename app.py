a9ddc55696ca1be5c32f0de67859c504b9fc3074
Welshy92 <>
2023-08-12 14:52:07 +0000

    Add house price study streamlit page.

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from app_pages.multipage import MultiPage
 
 # load page scripts first
 from app_pages.page_summary import page_summary_body
+from app_pages.house_price_study import house_price_study_body
 # from app_pages.page_project_hypothesis import page_project_hypothesis_body
 
 
@@ -10,6 +11,6 @@ app = MultiPage(app_name="Heritage")  # Creates an instance of the app
 
 # Adding app pages
 app.add_page("Quick Project Summary", page_summary_body)
-# app.add_page("Project Hypothesis and Validation", page_project_hypothesis_body)
+app.add_page("House Price Correlation study", house_price_study_body)
 
-app.run() # Run the  app
+app.run()  # Run the  app
